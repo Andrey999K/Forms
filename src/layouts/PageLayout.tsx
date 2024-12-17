36b59c7fcd389bd6f6e72c34c9e408fe5c9fe19d
Кутузов Андрей <a.kutuzov@2035.university>
2024-12-17 20:17:36 +0300

    feat: add link profile and exit in page layout

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,18 +1,62 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { Routes } from "../utils/routesConfig.ts";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { Routes } from "../utils/routesConfig.js";
+import { UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, MenuProps } from "antd";
+
+const pages = [
+  {
+    title: "Главная",
+    href: Routes.HOME,
+  },
+  {
+    title: "Новая форма",
+    href: Routes.FORMS_NEW,
+  },
+];
 
 export const PageLayout = () => {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate(Routes.LOGIN);
+  };
+
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: <NavLink to={Routes.ME}>Профиль</NavLink>,
+    },
+    {
+      key: "2",
+      label: <Button onClick={handleExit}>Выход</Button>,
+    },
+  ];
+
   return (
-    <div className="flex flex-col w-full">
-      <div className="p-5">
-        <div className="flex items-center justify-between gap-5">
-          <div className="flex items-center gap-3">
-            <NavLink to={Routes.HOME}>Главная</NavLink>
-            <NavLink to={Routes.FORMS_NEW}>Новая форма</NavLink>
+    <div className="min-h-screen">
+      <div className="p-5 flex items-center justify-center border-b-[1px] border-solid border-gray-200">
+        <div className="flex items-center gap-5 w-full max-w-screen-lg justify-between">
+          <div className="flex items-center gap-5">
+            {pages.map((page) => (
+              <NavLink
+                className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+                to={page.href}
+                key={page.href}
+              >
+                {page.title}
+              </NavLink>
+            ))}
           </div>
+          <Dropdown menu={{ items }} placement="bottomLeft">
+            <div className="flex justify-center items-center p-2 rounded-full bg-blue-400 cursor-pointer text-white">
+              <UserOutlined />
+            </div>
+          </Dropdown>
         </div>
       </div>
-      <Outlet />
+      <div className="mt-5 w-full max-w-screen-lg m-auto">
+        <Outlet />
+      </div>
     </div>
   );
 };
