546297e010d7c69ec2c253f9641a1ef77edab700
Daniil Rybakov <daniil18rusxxx@gmail.com>
2025-01-15 20:35:13 +0400

    Change design index page

--- src/components/Home/HomeCard/HomeCard.tsx
+++ src/components/Home/HomeCard/HomeCard.tsx
@@ -1,5 +1,8 @@
+import { ReactNode } from 'react';
 import { Card as CardType } from '@/types/card';
-import { Card, Button, Flex } from 'antd';
+import { Card } from 'antd';
+import { MdOutlineEdit, MdOutlineDelete } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 type Props = {
   item: CardType;
@@ -13,17 +16,33 @@ export const HomeCard = (props: Props) => {
     onDelete(item.id);
   };
 
+  const actions: ReactNode[] = [
+    <Link key="edit" to={`/forms/${item.id}/edit`}>
+      <MdOutlineEdit size={16} style={{ margin: '0 auto' }} href={`/forms/${item.id}/edit`} />
+    </Link>,
+    <div
+      key="delete"
+      className="text-red-600 hover:text-red-400 transition-all duration-200 ease-in-out"
+      onClick={handleDeleteClick}
+    >
+      <MdOutlineDelete size={16} style={{ margin: '0 auto' }} />
+    </div>,
+  ];
+
+  const responseText = item.responseCount ? `Отклики: ${item.responseCount}` : 'Откликов нет';
+
   return (
-    <Card title={item.title}>
-      <Flex gap={8}>
-        <Button href={`/forms/${item.id}/edit`} className="grow">
-          Изменить
-        </Button>
-        <Button className="grow" danger onClick={handleDeleteClick}>
-          Удалить
-        </Button>
-      </Flex>
-      <div className="mt-4">Отклики: {item.responseCount}</div>
+    <Card
+      title={item.title}
+      bordered={false}
+      actions={actions}
+      style={{ backdropFilter: 'blur(4px)', background: 'rgb(255 250 245 / 85%)' }}
+      styles={{
+        actions: { background: 'transparent', borderColor: '#ffe2cb' },
+        header: { borderColor: '#ffe2cb' },
+      }}
+    >
+      {responseText}
     </Card>
   );
 };
