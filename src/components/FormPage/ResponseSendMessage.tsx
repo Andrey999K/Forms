717f9d0e71a5182df828d6acf006b68f8851ea5c
Кутузов Андрей <a.kutuzov@2035.university>
2025-01-20 14:21:53 +0300

    feat(ResponseSendMessage): add GlassWrapper

--- src/components/FormPage/ResponseSendMessage.tsx
+++ src/components/FormPage/ResponseSendMessage.tsx
@@ -1,9 +1,10 @@
-import { Card, Result } from 'antd';
+import { Result } from 'antd';
+import { GlassWrapper } from '@/components/ui/wrapper/GlassWrapper.tsx';
 
 export const ResponseSendMessage = () => {
   return (
-    <Card>
+    <GlassWrapper>
       <Result status="success" title="Отклик успешно отправлен!" />
-    </Card>
+    </GlassWrapper>
   );
 };
