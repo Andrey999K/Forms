4c4460e46b2ab826ab7117e1eba58a6b08278358
Кутузов Андрей <a.kutuzov@2035.university>
2025-01-21 12:40:22 +0300

    feat(FormPage): add timer in FormPage

--- src/pages/formPage/index.tsx
+++ src/pages/formPage/index.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { ResponseSendMessage } from '@/components/FormPage';
 import { StartTimer } from '@/components/FormPage/StartTimer.tsx';
+import { Timer } from '@/components/FormPage/Timer.tsx';
 
 export const FormPage = () => {
   const { formId } = useParams();
@@ -33,7 +34,7 @@ export const FormPage = () => {
         return (
           <Radio.Group className="flex justify-start">
             {field.options?.map((option) => (
-              <Radio key={option.id} value={option.label}>
+              <Radio key={option.id} value={option.id}>
                 {option.label}
               </Radio>
             ))}
@@ -43,7 +44,7 @@ export const FormPage = () => {
         return (
           <Checkbox.Group className="flex justify-start">
             {field.options?.map((option) => (
-              <Checkbox key={option.id} value={option.label}>
+              <Checkbox key={option.id} value={option.id}>
                 {option.label}
               </Checkbox>
             ))}
@@ -52,12 +53,23 @@ export const FormPage = () => {
     }
   };
 
-  const onFinish = async (values: { [key: string]: string }) => {
+  const sendForm = async (values: { [key: string]: string | string[] }) => {
     const answers = Object.keys(values)
       .map((field) => {
         const questionData = formData?.fields.find((currentField) => currentField.id === field);
+        const findedField = formData?.fields.find((item) => item.id === field);
+        let value = values[field];
+        if (findedField?.type === 'radio') {
+          value = findedField.options?.find((option) => option.id === values[field])!
+            .label as string;
+        }
+        if (findedField?.type === 'checkbox' && Array.isArray(value)) {
+          value = value.map(
+            (item) => findedField.options?.find((option) => option.id === item)!.label as string
+          );
+        }
         return questionData
-          ? { id: questionData.id, question: questionData.question, answer: values[field] || '—' }
+          ? { id: questionData.id, question: questionData.question, answer: value || '—' }
           : false;
       })
       .filter((item) => item !== false);
@@ -78,6 +90,10 @@ export const FormPage = () => {
     }
   };
 
+  const onFinish = async (values: { [key: string]: string }) => {
+    await sendForm(values);
+  };
+
   // Обработчик изменения формы
   const onValuesChange = () => {
     // Проверяем наличие обязательных полей
@@ -93,6 +109,10 @@ export const FormPage = () => {
       return !!value;
     });
     setIsFormValid(isValid);
+  };
+
+  const sendFormAfterTimer = async () => {
+    await sendForm(form.getFieldsValue(true));
   };
 
   if (!formId) {
@@ -124,6 +144,7 @@ export const FormPage = () => {
         <>
           <h2 className="font-semibold text-lg">{formData.title}</h2>
           <p className="mt-3">{formData.description}</p>
+          {formData.settings?.timerActive && timerStart && <Timer onFinish={sendFormAfterTimer} />}
           <Form
             form={form}
             onFinish={onFinish}
