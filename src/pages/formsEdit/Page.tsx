980bb91d8a1e362a67f298853b523941d2b588a7
Кутузов Андрей <a.kutuzov@2035.university>
2025-01-29 16:53:30 +0300

    feat: add mobile version for form page

--- src/pages/formsEdit/Page.tsx
+++ src/pages/formsEdit/Page.tsx
@@ -20,7 +20,7 @@ import {
 import { getUUID } from '@/utils/getUUID';
 import { Spin } from 'antd';
 import { HTML5toTouch } from 'rdndmb-html5-to-touch';
-import { FC, useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { FC, useLayoutEffect, useMemo, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { MultiBackend } from 'react-dnd-multi-backend';
 import { useSelector } from 'react-redux';
@@ -157,10 +157,6 @@ export const FormsEdit: FC = () => {
       }));
     }
   }, [formData]);
-
-  useEffect(() => {
-    document.title = 'Конструктор';
-  }, []);
 
   if (isLoadingForm) {
     return (
