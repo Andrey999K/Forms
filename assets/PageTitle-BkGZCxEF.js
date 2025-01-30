b2e79010799158f59503a11531c41e66568b8d64
D1KANI <D1KANI@users.noreply.github.com>
2025-01-30 16:31:59 +0000

    deploy: 21fd950396589f0bc4962a66c2185dae77e7a3b8

---
+++ assets/PageTitle-BkGZCxEF.js
@@ -0,0 +1,1 @@
+import{bz as n,b as s}from"./index-lcoQv0vF.js";const c=({title:t})=>{const o=n();return s.useEffect(()=>{let e="Конструктор форм";t&&(e+=` | ${t}`),document.title=e},[o,t]),null};export{c as P};
