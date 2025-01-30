bc4cdfb0ff27005e0eaccc596b19e9709657a878
Daniil Rybakov <daniil18rusxxx@gmail.com>
2025-01-30 20:16:08 +0400

    Chore: test Pages with Browser router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import { PageLayout } from '@/layouts/PageLayout';
 import { lazy, Suspense } from 'react';
 import { ROUTES } from '@/utils/routesConfig';
@@ -162,7 +162,7 @@ export const AppRouter = () => {
   ];
 
   const routes = user ? authRoutes : notAuthRoutes;
-  const router = createHashRouter(routes);
+  const router = createBrowserRouter(routes);
 
   return <RouterProvider router={router} />;
 };
