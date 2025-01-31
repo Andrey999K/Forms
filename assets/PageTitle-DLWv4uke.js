9537cebd6fc2d2f534289651dcf4a864e546ce4f
AlexeyKharitonov <AlexeyKharitonov@users.noreply.github.com>
2025-01-31 16:06:10 +0000

    deploy: 56b7008c30fcd5362a5527c3be8575336f100597

---
+++ assets/PageTitle-DLWv4uke.js
@@ -0,0 +1,1 @@
+import{bJ as n,b as s}from"./index-D6DbJSyI.js";const c=({title:t})=>{const o=n();return s.useEffect(()=>{let e="Конструктор форм";t&&(e+=` | ${t}`),document.title=e},[o,t]),null};export{c as P};
--- assets/PageTitle-pgziiXI3.js
+++
@@ -1,1 +0,0 @@
-import{bz as n,b as s}from"./index-BdMAGVQa.js";const c=({title:t})=>{const o=n();return s.useEffect(()=>{let e="Конструктор форм";t&&(e+=` | ${t}`),document.title=e},[o,t]),null};export{c as P};
