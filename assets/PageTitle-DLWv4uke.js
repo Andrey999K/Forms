7915290f1fa4ba65a52ab354d23a7c4f9aa394f4
Bellusfennec <Bellusfennec@users.noreply.github.com>
2025-02-01 07:02:42 +0000

    deploy: e8c060c304b4a0fa359c3fd5bb9799c2081d60a4

--- assets/PageTitle-DLWv4uke.js
+++
@@ -1,1 +0,0 @@
-import{bJ as n,b as s}from"./index-D6DbJSyI.js";const c=({title:t})=>{const o=n();return s.useEffect(()=>{let e="Конструктор форм";t&&(e+=` | ${t}`),document.title=e},[o,t]),null};export{c as P};
---
+++ assets/PageTitle-Doxfeu0b.js
@@ -0,0 +1,1 @@
+import{bK as n,b as s}from"./index-DXFjYajV.js";const c=({title:t})=>{const o=n();return s.useEffect(()=>{let e="Конструктор форм";t&&(e+=` | ${t}`),document.title=e},[o,t]),null};export{c as P};
