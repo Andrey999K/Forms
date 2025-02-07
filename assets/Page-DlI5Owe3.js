65ac3394b23350a1076c8b457f0695e9397d88f5
D1KANI <D1KANI@users.noreply.github.com>
2025-02-07 16:12:42 +0000

    deploy: e490e9abc8c54754802c6acf0d13b8fa82dad1fb

--- assets/Page-DlI5Owe3.js
+++
@@ -1,1 +0,0 @@
-import{aq as a,b as r,ar as i,j as s,S as o}from"./index-CrfkSw2x.js";import{u as n}from"./usePageTitle-DdtFVwQg.js";const f=()=>{const t=a();return r.useEffect(()=>{const e=i();e&&t(`/forms/${e}/edit`,{state:{id:e}})},[]),n("Новая форма"),s.jsx("div",{className:"flex justify-center items-center",children:s.jsx(o,{})})};export{f as FormsNew};
