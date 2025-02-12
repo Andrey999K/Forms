125cd758e4cd1e6ddfc0776775a728d5788a3528
Andrey999K <Andrey999K@users.noreply.github.com>
2025-02-12 14:10:05 +0000

    deploy: c78ac3420262c0394238de5d1226d5651147b117

---
+++ assets/Page-BLwmFIdJ.js
@@ -0,0 +1,1 @@
+import{ap as a,b as r,aq as i,j as s,aj as o}from"./index-SZWxfF3G.js";import{u as m}from"./usePageTitle-CChf1kFY.js";const f=()=>{const t=a();return r.useEffect(()=>{const e=i();e&&t(`/forms/${e}/edit`,{state:{id:e,from:"/forms/new"},replace:!0})},[]),m("Новая форма"),s.jsx("div",{className:"flex justify-center items-center",children:s.jsx(o,{})})};export{f as FormsNew};
