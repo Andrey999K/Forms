b56332b9df41ad865d2ba6f8e5ea0534fd8baee8
Кутузов Андрей <a.kutuzov@2035.university>
2025-02-12 17:16:54 +0300

    fix: add ShapeWrapper in ErrorComponent

--- src/components/ui/ErrorComponent/ErrorComponent.tsx
+++ src/components/ui/ErrorComponent/ErrorComponent.tsx
@@ -1,4 +1,7 @@
 import { Button, Typography } from 'antd';
+import ShapeWrapper from '@/layouts/GlassLayout.tsx';
+
+const { Title, Text } = Typography;
 
 export const ErrorComponent = () => {
   const handleReload = () => {
@@ -6,14 +9,16 @@ export const ErrorComponent = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4 bg-gray-100">
-      <Typography.Title level={3} className="text-red-500">
-        Что-то пошло не так!
-      </Typography.Title>
-      <Typography.Text>Попробуйте перезагрузить страницу.</Typography.Text>
-      <Button type="primary" onClick={handleReload}>
-        Перезагрузить
-      </Button>
-    </div>
+    <ShapeWrapper>
+      <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <Title level={3} className="text-red-500 !mb-0">
+          Что-то пошло не так!
+        </Title>
+        <Text className="text-base mb-2">Попробуйте перезагрузить страницу.</Text>
+        <Button type="primary" onClick={handleReload}>
+          Перезагрузить
+        </Button>
+      </div>
+    </ShapeWrapper>
   );
 };
